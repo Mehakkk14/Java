6cadea2327ecc9848f787f5a9a0e4041eb653160
Mahak Rastogi <rastogimehak3845@gmail.com>
2026-09-06 17:43:05 +0530

    Added Arguments and Return type

--- src/Methods/Arguments.java
+++ src/Methods/Arguments.java
@@ -1,4 +1,11 @@
 package Methods;
 
 public class Arguments {
+    public static void main(String[] args) {
+        sum(4,7);
+    }
+
+    public static void sum(int a ,int b){
+        System.out.println(a+b);
+    }
 }
--- src/Methods/ReturnType.java
+++ src/Methods/ReturnType.java
@@ -1,4 +1,15 @@
 package Methods;
 
 public class ReturnType {
+    public static int work(){
+        System.out.println("Hello Hi");
+        System.out.println("How r u?");
+
+        return 10;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello Boss");
+        work();
+    }
 }
