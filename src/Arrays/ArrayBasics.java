80ccb69ca573712236034cff85748320ad8272b0
Mahak Rastogi <rastogimehak3845@gmail.com>
2026-09-12 19:00:30 +0530

    ArrayBasics

---
+++ src/Arrays/ArrayBasics.java
@@ -0,0 +1,30 @@
+package Arrays;
+import java.util.Arrays;
+
+public class ArrayBasics {
+    public static void main(String[] args) {
+
+        //Initialization method 1
+//        int[] x = {10,20,30,40,50,60,70};
+//
+//        //Indexing
+//        System.out.println(Arrays.toString(x));
+//        System.out.println(x[5]);
+//
+//        //Updating
+//        x[3] = 89;
+//        System.out.println(x[3]);
+
+        //Initialization method 2
+        int[] arr = new int[4];  // 4 size array | 0-3
+        arr[0] = 10;
+        arr[1] = 20;
+        arr[2] = 30;
+        arr[3] = 40;
+
+
+
+
+
+    }
+}
