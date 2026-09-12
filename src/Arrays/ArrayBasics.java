94d47d637329b013ccf951afbed9c531eded6d4a
Mahak Rastogi <159517775+Mehakkk14@users.noreply.github.com>
2026-09-12 19:01:40 +0530

    Update ArrayBasics.java

--- src/Arrays/ArrayBasics.java
+++ src/Arrays/ArrayBasics.java
@@ -4,16 +4,16 @@ import java.util.Arrays;
 public class ArrayBasics {
     public static void main(String[] args) {
 
-        //Initialization method 1
-//        int[] x = {10,20,30,40,50,60,70};
-//
-//        //Indexing
-//        System.out.println(Arrays.toString(x));
-//        System.out.println(x[5]);
-//
-//        //Updating
-//        x[3] = 89;
-//        System.out.println(x[3]);
+        // Initialization method 1
+       int[] x = {10,20,30,40,50,60,70};
+
+       //Indexing
+       System.out.println(Arrays.toString(x));
+       System.out.println(x[5]);
+
+       //Updating
+       x[3] = 89;
+       System.out.println(x[3]);
 
         //Initialization method 2
         int[] arr = new int[4];  // 4 size array | 0-3
