149295055e5f21376892df2a4fe11e598d0be3b0
Mahak Rastogi <rastogimehak3845@gmail.com>
2026-09-17 22:49:10 +0530

    Array

---
+++ src/Arrays/ArrayOfOtherDataTypes.java
@@ -0,0 +1,18 @@
+package Arrays;
+
+public class ArrayOfOtherDataTypes {
+    public static void main(String[] args) {
+//        double[] arr = new double[8];
+//        char [] arr = new char[5];
+//        for (int i=0; i<arr.length; i++){
+//
+//            System.out.print(arr[i]+" ");
+//        }
+
+        String[] arr = {"Hi", "Hello", "Namaste","Salaam"};
+        for (int i = 0; i< arr.length; i++){
+            System.out.println(arr[i]+" ");
+
+        }
+    }
+}
